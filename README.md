a6c3875c05282258abdecb54a225b9d3adc1ddd8
shivansh19-git <shivanshpathak82@gmail.com>
2026-02-26 13:46:50 +0530

    lexer completed

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # CPP-to-Python-Transpiler
 Developed a rule based C++ to Python transpiler that performs lexical analysis and syntax transformation to convert a subset of C++ programs into executable Python code.
+<br>
+We use **<sup>[1](#1) </sup>Regular Expressions (Regex)** to tokenize syntaxes, and convert by compling Regex's tokens.
+
+---
+
+<details>
+
+<summary>
+    <strong style="font-size: 1.5em;" id="1">
+        Regex : 
+    </strong>
+</summary>
+
+### Definition ➝
+Regex is a sequence of characters that defines a search pattern used to match, find, or manipulate text.<br>
+
+### Working ➝
+
+- Step 1 : Pattern Creation
+
+        Pattern is created like
+
+            (?P<IDENTIFIER>[a-zA-Z_]\w*) or 
+            (?P<NUMBER>\d+) etc..
+- Step 2 : Engine Processing
+
+        Pattern is compiled by Regex and then the engine scans the text left to right and tries to match the pattern.
+- Step 3 : Matching
+
+        If the pattern matches part of the string → match found
+        We access these matched members through self.regex.finditer():
+            match.lastgroup
+            match.group()
+</details>
+
+---
+  
--- lexer.py
+++ lexer.py
@@ -1,1 +1,76 @@
 # this document is used for tokenization of cpp code
+import re
+from dataclasses import dataclass
+
+# -----------------------------
+# TOKEN DEFINITION
+# -----------------------------
+
+@dataclass
+class Token:
+    type: str
+    value: str
+    line: int
+
+# -----------------------------
+# LEXER CLASS
+# -----------------------------
+
+class Lexer:
+    def __init__(self, code):
+        self.code = code
+        self.line = 1
+        self.tokens = []
+
+        # keywords are similar to identifiers, therefore another set which separates keywords.....
+        self.keywords = {
+            "int", "float", "double", "char",
+            "if", "else", "for", "while", "return",
+            "void", "using", "namespace"
+        }
+
+        # categorizing the data into the type they belong to........ (for creating regex patterns)
+        self.token_specification = [
+            ("COMMENT",   r"//.*"),
+            ("STRING",    r"\".*?\""),
+            ("FLOAT",     r"\d+\.\d+"),
+            ("NUMBER",    r"\d+"),
+            ("OPERATOR",  r"==|!=|<=|>=|\+\+|--|[+\-*/=<>]"),
+            ("DELIMITER", r"[;,\(\)\{\}]"),
+            ("IDENTIFIER",r"[A-Za-z_]\w*"),
+            ("NEWLINE",   r"\n"),
+            ("SKIP",      r"[ \t]+"),
+            ("MISMATCH",  r"."),
+        ]
+        # created a long regex string containing each possible pattern (we matched only few for a short project)
+        self.regex = re.compile(
+            "|".join(f"(?P<{name}>{pattern})" for name, pattern in self.token_specification)
+        )
+
+    # -----------------------------
+    # TOKENIZATION PROCESS
+    # -----------------------------
+
+    def tokenize(self):
+        # regex.finditer(self.code) scans the provided code and returns matches one by one (through loop), from regex (compiled above)
+        for match in self.regex.finditer(self.code):
+            kind = match.lastgroup
+            value = match.group()
+
+            if kind == "NEWLINE":
+                self.line += 1
+
+            elif kind == "SKIP" or kind == "COMMENT":
+                continue
+
+            elif kind == "IDENTIFIER" and value in self.keywords:
+                self.tokens.append(Token("KEYWORD", value, self.line))
+
+            elif kind == "MISMATCH":
+                raise RuntimeError(f"Unexpected character '{value}' at line {self.line}")
+
+            else:
+                self.tokens.append(Token(kind, value, self.line))
+
+        return self.tokens
+
